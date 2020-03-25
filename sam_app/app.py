4c88565651e4d72c1b59e2a573454ff81523c905
Gunjan Mehta <gunjan.mehta@mantelgroup.com.au>
2020-03-25 12:53:53 +1100

    edited aap.py

--- sam_app/app.py
+++ sam_app/app.py
@@ -1,12 +1,8 @@
-import os
 import json
 
 def lambda_handler(event, context):
-    if 'routeKey' in event:
-      str = json.dumps({
-            "message": "{} - hello world".format(os.environ["PREFIX"])
-        }) 
-    else:
-      str = '{} - hello world!'.format(os.environ['PREFIX'])
-    return str
-
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Warning from Lambda!')
+    }
